030682687a6558a66bce4424c7458e9db2cde9a5
praveen kumar <praveenkumar.tst@gmail.com>
2023-11-08 20:04:16 +0530

    added redirectores

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
 echo -e "\e[31m *****install nginx****** \e[0m"
-dnf install nginx -y
+dnf install nginx -y &>>/tmp/expense.log
 
 echo -e "\e[31m *****copy expense configuration file ****** \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
 echo -e "\e[31m *****remove nginx content****** \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
 echo -e "\e[31m *****download the content Zip file****** \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[31m *****Extract the downloaded zip file ****** \e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>/tmp/expense.log
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 
 echo -e "\e[31m *****Enable and restart nginx****** \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>/tmp/expense.log
+systemctl restart nginx &>>/tmp/expense.log
