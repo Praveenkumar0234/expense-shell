5746ff8e1530a1e3109dc11c841f39f18fd4e31a
praveen kumar <praveenkumar.tst@gmail.com>
2023-11-08 21:15:27 +0530

    added redirectores

--- backend.sh
+++ backend.sh
@@ -1,33 +1,48 @@
-echo -e "\e[33m ******disable old version nodejs and enable 18 version nodejs***** \e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+log_file="/tmp/expense.log"
+colour="\e[33m"
 
-echo -e "\e[33m ******install nodejs***** \e[0m"
-dnf install nodejs -y
+echo -e "${colour} ******disable old version nodejs and enable 18 version nodejs***** \e[0m"
+dnf module disable nodejs -y &>>${log_file}
+dnf module enable nodejs:18 -y &>>${log_file}
+echo $?
 
-echo -e "\e[33m ******copy service file***** \e[0m"
-cp backend.service /etc/systemd/system/backend.service
+echo -e "${colour} ******install nodejs***** \e[0m"
+dnf install nodejs -y &>>${log_file}
+echo $?
 
-echo -e "\e[33m ******add application user***** \e[0m"
-useradd expense
+echo -e "${colour} ******copy service file***** \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>${log_file}
+echo $?
 
-echo -e "\e[33m ******Create app directory ***** \e[0m"
-mkdir /app
+echo -e "${colour} ******add application user***** \e[0m"
+useradd expense &>>/${log_file}
+echo $?
 
-echo -e "\e[33m ******downloading and extracting the  application code***** \e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
+echo -e "${colour} ******Create app directory ***** \e[0m"
+mkdir /app &>>${log_file}
+echo $?
 
-echo -e "\e[33m ******install nodejs dependencies***** \e[0m"
-cd /app
-npm install
+echo -e "${colour} ******delete all the application content***** \e[0m"
+rm -rf /app* &>>${log_file}
+echo $?
 
-echo -e "\e[33m ******install mysql client & load schema***** \e[0m"
-dnf install mysql -y
-mysql -h mysql-dev.praveendevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo -e "${colour} ******downloading and extracting the  application code***** \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>${log_file}
+cd /app &>>${log_file}
+unzip /tmp/backend.zip &>>${log_file}
+echo $?
 
-echo -e "\e[33m ******reload, enable and restart service ***** \e[0m"
-systemctl daemon-reload
-systemctl enable backend
-systemctl restart backend
+echo -e "${colour} ******install nodejs dependencies***** \e[0m"
+npm install &>>${log_file}
+echo $?
+
+echo -e "${colour} ******install mysql client & load schema***** \e[0m"
+dnf install mysql -y &>>${log_file}
+mysql -h mysql-dev.praveendevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>${log_file}
+echo $?
+
+echo -e "${colour} ******reload, enable and restart service ***** \e[0m"
+systemctl daemon-reload &>>${log_file}
+systemctl enable backend &>>${log_file}
+systemctl restart backend &>>${log_file}
+echo $?
--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,28 @@
-echo -e "\e[31m *****install nginx****** \e[0m"
-dnf install nginx -y &>>/tmp/expense.log
+log_file="/tmp/expense.log"
+colour="\e[31m"
 
-echo -e "\e[31m *****copy expense configuration file ****** \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+echo -e "${colour} *****install nginx****** \e[0m"
+dnf install nginx -y &>>${log_file}
+echo $?
 
-echo -e "\e[31m *****remove nginx content****** \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+echo -e "${colour} *****copy expense configuration file ****** \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>${log_file}
+echo $?
 
-echo -e "\e[31m *****download the content Zip file****** \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+echo -e "${colour} *****remove nginx content****** \e[0m"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+echo $?
 
-echo -e "\e[31m *****Extract the downloaded zip file ****** \e[0m"
-cd /usr/share/nginx/html &>>/tmp/expense.log
-unzip /tmp/frontend.zip &>>/tmp/expense.log
+echo -e "${colour} *****download the content Zip file****** \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+echo $?
 
-echo -e "\e[31m *****Enable and restart nginx****** \e[0m"
-systemctl enable nginx &>>/tmp/expense.log
-systemctl restart nginx &>>/tmp/expense.log
+echo -e "${colour} *****Extract the downloaded zip file ****** \e[0m"
+cd /usr/share/nginx/html &>>${log_file}
+unzip /tmp/frontend.zip &>>${log_file}
+echo $?
+
+echo -e "${colour} *****Enable and restart nginx****** \e[0m"
+systemctl enable nginx &>>${log_file}
+systemctl restart nginx &>>${log_file}
+echo $?
