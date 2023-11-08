84258af0fd9a9977813f4e750120fb1130cf1060
praveen kumar <praveenkumar.tst@gmail.com>
2023-11-08 13:31:07 +0530

    Added steps in mysql

---
+++ mysql.sh
@@ -0,0 +1,15 @@
+echo -e "\e[32m *****disable mysql 8 version***** \e[0m"
+dnf module disable mysql -y
+
+echo -e "\e[32m *****cpy the repo file ***** \e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[32m *****install mysql 5.7***** \e[0m"
+dnf install mysql-community-server -y
+
+echo -e "\e[32m *****Setup the password***** \e[0m"
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
+echo -e "\e[32m *****enable and restart mysql***** \e[0m"
+systemctl enable mysqld
+systemctl restart mysqld
