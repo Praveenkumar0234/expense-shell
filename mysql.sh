4a21ec5c8d4df6e2d84f45bf4114260205b5d54d
praveen kumar <praveenkumar.tst@gmail.com>
2023-11-08 14:22:55 +0530

    changes in mysql

--- mysql.sh
+++ mysql.sh
@@ -7,9 +7,10 @@ cp mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[32m *****install mysql 5.7***** \e[0m"
 dnf install mysql-community-server -y
 
-echo -e "\e[32m *****Setup the password***** \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1
 
 echo -e "\e[32m *****enable and restart mysql***** \e[0m"
 systemctl enable mysqld
 systemctl restart mysqld
+
+echo -e "\e[32m *****Setup the password***** \e[0m"
+mysql_secure_installation --set-root-pass ExpenseApp@1
