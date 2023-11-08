77af0891f70699020ffc33d55dc66ca322e2647a
praveen kumar <praveenkumar.tst@gmail.com>
2023-11-08 13:19:25 +0530

    added print command for all the steps

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,19 @@
+echo -e "\e[31m *****install nginx****** \e[0m"
 dnf install nginx -y
 
+echo -e "\e[31m *****copy expense configuration file ****** \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
+echo -e "\e[31m *****remove nginx content****** \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[31m *****download the content Zip file****** \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[31m *****Extract the downloaded zip file ****** \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "\e[31m *****Enable and restart nginx****** \e[0m"
 systemctl enable nginx
 systemctl restart nginx
